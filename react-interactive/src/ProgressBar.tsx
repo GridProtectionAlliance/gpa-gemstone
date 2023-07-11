b125c79b8942581b1220b8c3b67e3ed577c481e1
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2023-07-11 09:33:13 -0400

    Cleaned up Progressbar

--- react-interactive/src/ProgressBar.tsx
+++ react-interactive/src/ProgressBar.tsx
@@ -1,7 +1,7 @@
 // ******************************************************************************************************
 //  Step.tsx - Gbtc
 //
-//  Copyright © 2020, Grid Protection Alliance.  All Rights Reserved.
+//  Copyright ï¿½ 2020, Grid Protection Alliance.  All Rights Reserved.
 //
 //  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
 //  the NOTICE file distributed with this work for additional information regarding copyright ownership.
@@ -23,23 +23,46 @@
 
 import * as React from 'react'
 
+/// Styles for all Circles
+const circleStyle: React.CSSProperties = {
+    height: '25px',
+    width: '25px',
+    borderRadius: '50%',
+    borderWidth: '2px',
+    borderStyle: 'solid',
+    display: 'inline-block',
+    backgroundColor: '#fff',
+}
 
+interface IStep {
+    short: string,
+    long: string,
+    id: string | number
+}
 interface IProps {
-    steps:
-    {
-        short: string,
-        long: string,
-        id: string | number
-    }[],
+    steps: IStep[],
     activeStep: string | number,
     height?: string | number,
     width?: string | number
 }
 
 function ProgressBar(props: IProps) {
-    let met = false;
-    let metIndex = 0;
-    const divs: JSX.Element[] = [];
+    const [activeStep, setActiveStep] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        if (props.steps.length < 2) {
+            setActiveStep(0);
+            return;
+        }
+
+        const index = props.steps.findIndex(s => s.id == props.activeStep);
+
+        if (index == -1)
+            setActiveStep(0);
+        else
+            setActiveStep(index);
+
+    }, [props.activeStep, props.steps])
 
     /// Styles for overall div
     const stepsStyle: React.CSSProperties = {
@@ -47,7 +70,6 @@ function ProgressBar(props: IProps) {
         minHeight: '210px',
         width: props.width === undefined ? '100%' : props.width,
         minWidth: '210px',
-        position: 'absolute',
         justifyContent: 'space-evenly',
 
     }
@@ -56,70 +78,57 @@ function ProgressBar(props: IProps) {
     const stepsContainerStyle: React.CSSProperties = {
         width: '100%',
         backgroundColor: '#DDD',
-        maxHeight: '10px',
-        height: '10%',
-        position: 'absolute',
+        height: '10px',
         top: '50%',
         borderRadius: '10px 0 0 10px'
-    }
-
-    /// Styles for past/current circles
-    const activeCircleStyle: React.CSSProperties = {
-        height: '25px',
-        width: '25px',
-        backgroundColor: '#fff',
-        borderRadius: '50%',
-        borderWidth: '2px',
-        borderStyle: 'solid',
-        borderColor: '#5DC177',
-        display: 'inline-block'
-    }
-
-    /// Styles for unactivated circles
-    const inactiveCircleStyle: React.CSSProperties = {
-        height: '25px',
-        width: '25px',
-        backgroundColor: '#fff',
-        borderRadius: '50%',
-        borderWidth: '2px',
-        borderStyle: 'solid',
-        borderColor: '#D3D3D3',
-        display: 'inline-block'
     }
 
     const descriptionStyles: React.CSSProperties = {
         fontSize: '15px',
         fontStyle: 'italic',
         color: '#538897',
-        alignContent: 'center',
+        width: '100%',
         display: 'inline-block'
     }
 
-    /// Create each circle and description
-    function getSteps() {
-        props.steps.forEach((x) => {
-
-            if (x.id === props.activeStep) {
-                met = true;
-                metIndex = props.steps.indexOf(x);
-            }
-
-            divs.push(
-                <div style={{ height: '60px', width: '25px', alignContent: 'center' }}><span style={x.id === props.activeStep || !met ? activeCircleStyle : inactiveCircleStyle}></span><span style={descriptionStyles}>{x.id === props.activeStep ? x.long : x.short}</span></div>
-            );
-
-
-        });
-
-    }
-
-    { getSteps() }
     return (
-        <div  id='steps' style={stepsStyle}>
-            <div style={stepsContainerStyle}></div>
-            <div style={{ width: ((metIndex / (props.steps.length - 1)) * 100).toString() + '%', backgroundColor: '#5DC177', maxHeight: '10px', height: '10%', position: 'absolute', top: '50%', borderRadius: '10px 0 0 10px' }}></div>
-            <div style={{ height: '100px', width: '100%', top: '46%', position: 'absolute', display: 'flex', justifyContent: 'space-between' }}>
-                {divs}
+        <div id='steps' style={stepsStyle}>
+            <div style={stepsContainerStyle} />
+            <div style={{
+                width: ((activeStep / (props.steps.length - 1)) * 100).toString() + '%',
+                backgroundColor: '#5DC177',
+                height: 10,
+                top: '50%',
+                borderRadius: '10px 0 0 10px',
+                marginTop: -10
+            }}></div>
+            <div style={{
+                height: '100px',
+                width: '100%',
+                top: '46%',
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}>
+                {props.steps.map((x,i) => <div
+                    style={{
+                        height: '60px',
+                        width: 'calc(' + (100/props.steps.length).toString() + '%)',
+                        marginTop: -17
+                    }}>
+                    <span style={{
+                        ...circleStyle,
+                        borderColor: i <= activeStep ? '#5DC177' : '#D3D3D3',
+                        marginLeft: (i == (props.steps.length-1) ? 'calc(100% - 25px)' : (i==0? undefined : 'calc(50% - 12px)')),
+                        marginRight: (i == 0 ? 'calc(100% - 25px)' : (i == (props.steps.length - 1) ? undefined :'calc(50% - 12px)'))
+                    }} />
+                    <span style={{
+                        ...descriptionStyles,
+                        textAlign: (i == 0? 'left' : (i == (props.steps.length - 1) ? 'right' : 'center'))
+                    }}>
+                        {x.id === props.activeStep ? x.long : x.short}
+                    </span>
+                </div>)
+}
             </div>
         </div>
     );
