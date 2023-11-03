81526e46497159ef3d5128722906d686f9007b11
Christoph Lackner <clackner@gridprotectionalliance.org>
2023-11-03 15:34:40 -0400

    fixed cursor jumping in TextArea

--- react-forms/src/TextArea.tsx
+++ react-forms/src/TextArea.tsx
@@ -18,6 +18,8 @@
 //  ----------------------------------------------------------------------------------------------------
 //  01/22/2020 - Billy Ernest
 //       Generated original version of source code.
+//  11/03/2023 - C Lackner
+//       Added internal state to avoid cursor jumping.
 //
 // ******************************************************************************************************
 import * as React from 'react';
@@ -25,56 +27,65 @@ import { CreateGuid } from '@gpa-gemstone/helper-functions'
 import HelperMessage from './HelperMessage';
 
 interface IProps<T> {
-    Rows: number;
-    Record: T;
-    Field: keyof T;
-    Setter: (record: T) => void;
-    Valid: (field: keyof T) => boolean;
-    Label?: string;
-    Feedback?: string;
-    Disabled?: boolean;
-    Help?: string | JSX.Element;
+  Rows: number;
+  Record: T;
+  Field: keyof T;
+  Setter: (record: T) => void;
+  Valid: (field: keyof T) => boolean;
+  Label?: string;
+  Feedback?: string;
+  Disabled?: boolean;
+  Help?: string | JSX.Element;
 }
 
 export default function TextArea<T>(props: IProps<T>) {
-    const [guid, setGuid] = React.useState<string>("");
-    const [showHelp, setShowHelp] = React.useState<boolean>(false);
+  const [guid, setGuid] = React.useState<string>("");
+  const [showHelp, setShowHelp] = React.useState<boolean>(false);
+  const [internal, setInternal] = React.useState<boolean>(false);
+  const [heldVal, setHeldVal] = React.useState<string>('');
+  
+  React.useEffect(() => {
+    if (!internal) {
+      setHeldVal(props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString());
+    }
+    setInternal(false);
+   }, [props.Record[props.Field]]);
 
-    React.useEffect(() => {
-        setGuid(CreateGuid());
-    }, []);
+  React.useEffect(() => {
+      setGuid(CreateGuid());
+  }, []);
 
-    const showLabel = props.Label !== "";
-    const showHelpIcon = props.Help !== undefined;
-    const label = props.Label === undefined ? props.Field : props.Label;
+  function valueChange(value: string) {
+    setInternal(true);
+    props.Setter({ ...props.Record, [props.Field]: value !== '' ? value : null });
+    setHeldVal(value);
+   }
 
-    return (
-        <div className="form-group" data-help={guid}>
-            {showHelpIcon || showLabel ?
-                <label>{showLabel ? label : ''}
-                    {showHelpIcon ? <div style={{ width: 20, height: 20, borderRadius: '50%', display: 'inline-block', background: '#0D6EFD', marginLeft: 10, textAlign: 'center', fontWeight: 'bold' }} onMouseEnter={() => setShowHelp(true)} onMouseLeave={() => setShowHelp(false)}> ? </div> : null}
-                </label> : null}
-            {showHelpIcon ?
-                <HelperMessage Show={showHelp} Target={guid}>
-                    {props.Help}
-                </HelperMessage>
-                : null}
-            <textarea
-                rows={props.Rows}
-                className={props.Valid(props.Field) ? 'form-control' : 'form-control is-invalid'}
-                onChange={(evt) => {
-                    const record: T = { ...props.Record };
-                    if (evt.target.value !== '') record[props.Field] = evt.target.value as any;
-                    else record[props.Field] = null as any;
+  const showLabel = props.Label !== "";
+  const showHelpIcon = props.Help !== undefined;
+  const label = props.Label === undefined ? props.Field : props.Label;
 
-                    props.Setter(record);
-                }}
-                value={props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString()}
-                disabled={props.Disabled == null ? false : props.Disabled}
-            />
-            <div className="invalid-feedback">
-                {props.Feedback == null ? props.Field + ' is a required field.' : props.Feedback}
-            </div>
-        </div>
-    );
+  return (
+    <div className="form-group" data-help={guid}>
+    {showHelpIcon || showLabel ?
+        <label>{showLabel ? label : ''}
+            {showHelpIcon ? <div style={{ width: 20, height: 20, borderRadius: '50%', display: 'inline-block', background: '#0D6EFD', marginLeft: 10, textAlign: 'center', fontWeight: 'bold' }} onMouseEnter={() => setShowHelp(true)} onMouseLeave={() => setShowHelp(false)}> ? </div> : null}
+        </label> : null}
+    {showHelpIcon ?
+        <HelperMessage Show={showHelp} Target={guid}>
+            {props.Help}
+        </HelperMessage>
+        : null}
+    <textarea
+        rows={props.Rows}
+        className={props.Valid(props.Field) ? 'form-control' : 'form-control is-invalid'}
+        onChange={(evt) => valueChange(evt.target.value)}
+        value={heldVal == null ? '' : heldVal}
+        disabled={props.Disabled == null ? false : props.Disabled}
+    />
+    <div className="invalid-feedback">
+        {props.Feedback == null ? props.Field + ' is a required field.' : props.Feedback}
+    </div>
+</div>
+);
 }
