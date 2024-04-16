e7b74264097beaf41c68af74368f85ad4d6f4943
prestoncraw <prestoncrawford530@gmail.com>
2024-04-16 11:29:44 -0400

    Cast to unknown before cast to boolean

--- react-forms/src/CheckBox.tsx
+++ react-forms/src/CheckBox.tsx
@@ -55,8 +55,8 @@ export default function CheckBox<T>(props: IProps<T>) {
             record[props.Field] = evt.target.checked as any;
             props.Setter(record);
           }}
-          value={(props.Record[props.Field] as boolean) ? 'on' : 'off'}
-          checked={(props.Record[props.Field] as boolean)}
+          value={(props.Record[props.Field] as unknown as boolean) ? 'on' : 'off'}
+          checked={(props.Record[props.Field] as unknown as boolean)}
           disabled={props.Disabled == null ? false : props.Disabled}
         />
         <label className="form-check-label">{props.Label == null ? props.Field : props.Label}</label>
