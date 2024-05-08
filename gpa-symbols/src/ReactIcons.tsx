56146674995cca99a99b07b801e7ffbe69f19332
prestoncraw <prestoncrawford530@gmail.com>
2024-05-08 09:28:46 -0400

    Added warning and checkmark to reacticons

--- gpa-symbols/src/ReactIcons.tsx
+++ gpa-symbols/src/ReactIcons.tsx
@@ -168,4 +168,16 @@ export namespace ReactIcons {
             <line x1="12" y1="5" x2="12" y2="19"></line>
             <line x1="5" y1="12" x2="19" y2="12"></line>
         </svg>
+
+    export const Warning: React.FC<IProps> = (props) =>
+        <svg xmlns="http://www.w3.org/2000/svg" width={props.Size ?? 24} height={props.Size ?? 24} viewBox="0 0 24 24" fill="none" stroke={props.Color ?? "currentColor"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-alert-triangle">
+            <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+            <line x1="12" y1="9" x2="12" y2="13"></line>
+            <line x1="12" y1="17" x2="12.01" y2="17"></line>
+        </svg>
+
+    export const CheckMark: React.FC<IProps> = (props) =>
+        <svg xmlns="http://www.w3.org/2000/svg" width={props.Size ?? 24} height={props.Size ?? 24} viewBox="0 0 24 24" fill="none" stroke={props.Color ?? "currentColor"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check">
+            <polyline points="20 6 9 17 4 12"></polyline>
+        </svg>
 }
