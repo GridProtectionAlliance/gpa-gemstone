83d9e1371bc13d6f8a5756e359199423efa62160
prestoncraw <prestoncrawford530@gmail.com>
2024-05-16 11:10:00 -0400

    Rework ColorPicker and fix positioning bug

--- react-forms/src/ColorPicker.tsx
+++ react-forms/src/ColorPicker.tsx
@@ -22,17 +22,22 @@
 //******************************************************************************************************
 
 import * as React from 'react';
-import { BlockPicker, ColorResult } from 'react-color';
+import { BlockPicker, Color, ColorResult } from 'react-color';
 import styled from "styled-components";
 import { GetNodeSize } from '@gpa-gemstone/helper-functions'
 import { Portal } from 'react-portal';
+import { isEqual } from 'lodash';
 
-interface IProps {
-    CurrentColor: string,
+interface IProps<T> {
+    Record: T;
+    Field: keyof T;
+    Setter: (record: T, color: ColorResult) => void;
+    Label: string;
+    Disabled?: boolean;
+    Feedback?: string;
+    Style?: React.CSSProperties;
     Colors?: string[],
-    OnColorChange: (updatedColor: ColorResult) => void,
     Triangle?: ('hide' | 'top'),
-    BtnText?: string
 }
 
 interface IWrapperProps {
@@ -60,103 +65,102 @@ const WrapperDiv = styled.div<IWrapperProps>`
   }
 `
 
-const ColorPicker: React.FunctionComponent<IProps> = (props) => {
-    const toolTip = React.useRef(null);
+interface ISize {
+    Top: number,
+    Left: number,
+    Width: number,
+    Height: number
+}
+
+export default function ColorPicker<T>(props: IProps<T>) {
+    const toolTipRef = React.useRef<HTMLDivElement>(null);
+    const buttonRef = React.useRef<HTMLButtonElement>(null);
 
     const [top, setTop] = React.useState<number>(0);
     const [left, setLeft] = React.useState<number>(0);
 
-    const [targetLeft, setTargetLeft] = React.useState<number>(0);
-    const [targetTop, setTargetTop] = React.useState<number>(0);
-    const [targetWidth, setTargetWidth] = React.useState<number>(0);
-    const [targetHeight, setTargetHeight] = React.useState<number>(0);
+    const [targetPosition, setTargetPosition] = React.useState<ISize>({ Top: -999, Left: -999, Width: 0, Height: 0 })
+
     const [show, setShow] = React.useState<boolean>(false)
-    const colorsArray = props.Colors ?? ["#A30000", "#0029A3", "#007A29", "#d3d3d3", "#edc240",
+    const colors = props.Colors ?? ["#A30000", "#0029A3", "#007A29", "#d3d3d3", "#edc240",
         "#afd8f8", "#cb4b4b", "#4da74d", "#9440ed", "#BD9B33", "#EE2E2F",
         "#008C48", "#185AA9", "#F47D23", "#662C91", "#A21D21", "#B43894",
         "#737373", "#ff904f", "#ff9999"]
 
-    React.useEffect(() => {
-        const target = document.querySelectorAll(`[data-tooltip${`="color-picker"`}]`)
-
-        if (target.length === 0) {
-            setTargetHeight(0);
-            setTargetWidth(0);
-            setTargetLeft(-999);
-            setTargetTop(-999);
-        }
-        else {
-            const targetLocation = GetNodeSize(target[0] as HTMLElement);
-            setTargetHeight(targetLocation.height);
-            setTargetWidth(targetLocation.width);
-            setTargetLeft(targetLocation.left);
-            setTargetTop(targetLocation.top);
-        }
-    }, []);
-
     React.useLayoutEffect(() => {
-        const [t, l] = UpdatePosition();
-        setTop(t);
-        setLeft(l);
-    });
+        if (buttonRef.current === null) return;
+        const targetLocation = GetNodeSize(buttonRef.current);
+        const newTargetPosition = { Top: targetLocation.top, Left: targetLocation.left, Height: targetLocation.height, Width: targetLocation.width }
 
-    function UpdatePosition() {
-
-        if (toolTip.current === null)
-            return [-999, -999];
-
-        const colorPickerHeight = 280
-        const colorPickerWidth = 180
-        const tipLocation = GetNodeSize(toolTip.current);
-        const windowHeight = window.innerHeight
-        const windowWidth = window.innerWidth
-        const result: [number, number] = [0, 0];
-
-        //start by placing at the bottom of the button and rotating counter clockwise until there is enough space to fit the colorpicker
-        result[0] = targetTop + targetHeight;
-        result[1] = targetLeft + 0.5 * targetWidth - 0.5 * tipLocation.width;
-
-        //try to place colorpicker to the right of the button if there isnt enough room below
-        if (windowHeight - result[0] < colorPickerHeight) {
-            result[0] = targetTop + 0.5 * targetHeight - 0.5 * tipLocation.height
-            result[1] = targetLeft + targetWidth;
-
-            //try to place colorpicker on the top of the button if there isnt enough room to the right
-            if (windowWidth - result[1] < colorPickerWidth) {
-                result[0] = targetTop - tipLocation.height;
-                result[1] = targetLeft + 0.5 * targetWidth - 0.5 * tipLocation.width;
-
-                //move colorpicker to the left of the button if there isnt enough space on the top
-                if (result[0] < 0) {
-                    result[0] = targetTop + 0.5 * targetHeight - 0.5 * tipLocation.height;
-                    result[1] = targetLeft - tipLocation.width;
-                }
-            }
+        if (!isEqual(newTargetPosition, targetPosition)) {
+            setTargetPosition(newTargetPosition);
+            const [t, l] = GetBestPosition(toolTipRef, targetLocation.top, targetLocation.height, targetLocation.left, targetLocation.width);
+            setTop(t);
+            setLeft(l);
         }
-
-        return result;
-    }
+    });
 
     return (
         <>
-            <button className="btn btn-block" data-tooltip={"color-picker"} onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)} style={{ backgroundColor: props.CurrentColor, borderColor: props.CurrentColor }}>
-                {props.BtnText ?? "Color"}
+            <button disabled={props.Disabled ?? false} ref={buttonRef} className="btn btn-block" data-tooltip={"color-picker"} onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)} style={props.Style}>
+                {props.Label ?? ""}
             </button>
+            {props.Feedback !== undefined && (props.Disabled ?? false) ?
+                <div className="text-danger">
+                    {props.Feedback}
+                </div> : null}
             <Portal>
-                <WrapperDiv Show={show} Top={top} Left={left} ref={toolTip} onMouseOver={() => setShow(true)} onMouseOut={() => setShow(false)}>
-                    <BlockPicker
-                        color={props.CurrentColor}
-                        colors={colorsArray}
-                        onChangeComplete={(updatedColor) => {
-                            props.OnColorChange(updatedColor);
-                        }}
-                        triangle={props.Triangle ?? 'hide'}
-                    />
-                </WrapperDiv>
+                {!(props.Disabled ?? false) ?
+                    <WrapperDiv Show={show} Top={top} Left={left} ref={toolTipRef} onMouseOver={() => (props.Disabled ?? false) ? {} : setShow(true)} onMouseOut={() => setShow(false)}>
+                        <BlockPicker
+                            color={props.Record[props.Field] as unknown as Color}
+                            colors={colors}
+                            onChangeComplete={(updatedColor) => {
+                                const record: T = { ...props.Record };
+                                record[props.Field] = updatedColor.hex as any
+                                props.Setter(record, updatedColor);
+                            }}
+                            triangle={props.Triangle ?? 'hide'}
+                        />
+                    </WrapperDiv>
+                    : <></>}
             </Portal>
         </>
     )
-
 }
 
-export default ColorPicker;
+const GetBestPosition = (ref: React.RefObject<HTMLDivElement>, targetTop: number, targetHeight: number, targetLeft: number, targetWidth: number) => {
+    if (ref.current === null)
+        return [-999, -999];
+
+    const colorPickerHeight = 280
+    const colorPickerWidth = 180
+    const tipLocation = GetNodeSize(ref.current);
+    const windowHeight = window.innerHeight
+    const windowWidth = window.innerWidth
+    const result: [number, number] = [0, 0];
+
+    //start by placing at the bottom of the button and rotating counter clockwise until there is enough space to fit the colorpicker
+    result[0] = targetTop + targetHeight;
+    result[1] = targetLeft + 0.5 * targetWidth - 0.5 * tipLocation.width;
+
+    //try to place colorpicker to the right of the button if there isnt enough room below
+    if (windowHeight - result[0] < colorPickerHeight) {
+        result[0] = targetTop + 0.5 * targetHeight - 0.5 * tipLocation.height
+        result[1] = targetLeft + targetWidth;
+
+        //try to place colorpicker on the top of the button if there isnt enough room to the right
+        if (windowWidth - result[1] < colorPickerWidth) {
+            result[0] = targetTop - tipLocation.height;
+            result[1] = targetLeft + 0.5 * targetWidth - 0.5 * tipLocation.width;
+
+            //move colorpicker to the left of the button if there isnt enough space on the top
+            if (result[0] < 0) {
+                result[0] = targetTop + 0.5 * targetHeight - 0.5 * tipLocation.height;
+                result[1] = targetLeft - tipLocation.width;
+            }
+        }
+    }
+
+    return result;
+}
