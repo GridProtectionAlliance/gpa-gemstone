ca7c5f0b3066a9ac8b1321e7eef0e2e443d93320
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-05-31 16:28:32 -0400

    Refactored React Table

--- react-table/src/AdjustableTable/AdjustableColumn.tsx
+++ react-table/src/AdjustableTable/AdjustableColumn.tsx
@@ -18,89 +18,125 @@
 //  ----------------------------------------------------------------------------------------------------
 //  11/19/2023 - C. Lackner
 //       Generated original version of source code.
+//  05/31/2024 - C. Lackner
+//       Refactored to fix sizing issues.
 //
 //  ******************************************************************************************************
-
 import { SVGIcons } from '@gpa-gemstone/gpa-symbols';
 import { GetNodeSize } from '@gpa-gemstone/helper-functions';
 import * as React from 'react';
-import { IColumnProps } from './Column';
+import { IColumnProps, IDataWrapperProps, IHeaderWrapperProps } from './Column';
 
 
-interface IAdjustableColProps<T> extends IColumnProps<T> {
-    /**
-     * The minimum Width used for this Collumn - default is 100px
-     */
-    MinWidth?: number    
-}
-
-
-export default function AdjustableCol<T>(props: React.PropsWithChildren<IAdjustableColProps<T>>) {
+export default function AdjustableColumn<T>(props: React.PropsWithChildren<IColumnProps<T>>) {
     return <>{props.children}</>
 }
 
 
-interface IHeaderWrapperProps<T> extends IAdjustableColProps<T> {
-    setWidth: (w: number, m: number) => void,
-    width?: number,
-    onSort: (key: string, event: any, field?: keyof T) => void,
-    sorted: boolean,
-    asc: boolean,
-    startAdjustment: (e: any) => void;
-    fixedLayout: boolean
-} 
+interface IAdjustableHeaderWrapperProps extends IHeaderWrapperProps {
+    adjustment: number,
+    startAdjustment: (e: any) => void,
+    setMinWidth: (w: number) => void,
+    minWidth?: number,
+    setMaxWidth: (w: number) => void,
+    maxWidth?: number,
+}
 
-export function AdjColumnHeaderWrapper<T>(props: React.PropsWithChildren<IHeaderWrapperProps<T>>) {
+export function AdjustableColumnHeaderWrapper(props: React.PropsWithChildren<IAdjustableHeaderWrapperProps>) {
     const thref = React.useRef(null);
+    const mode = React.useRef<'width' | 'minWidth' | 'maxWidth'>('minWidth');
+
     const [showBorder, setShowBorder] = React.useState(false);
-    
+
     const onHover = React.useCallback(() => { setShowBorder(true); }, [])
     const onLeave = React.useCallback(() => { setShowBorder(false); }, [])
 
-    const style = (props.RowStyle !== undefined) ? {...props.HeaderStyle } : { };
+    const style = (props.style !== undefined) ? { ...props.style } : {};
 
-    if (props.width !== undefined && props.fixedLayout){
-        style.width = props.width;
-        style.minWidth = props.width;
-        style.maxWidth = props.width;
+    style.overflowX = style.overflowX ?? 'hidden';
+    style.display = style.display ?? 'inline-block'
+    style.position = style.position ?? 'relative';
+    style.borderTop = style.borderTop ?? 'none';
+    style.width = style.width ?? props.width;
+    style.minWidth = style.minWidth ?? 100;
+
+    if (mode.current === 'minWidth') {
+        style.width = style.minWidth;
     }
 
-    if (style.cursor === undefined && (props.AllowSort ?? true)) {
+    if (mode.current === 'maxWidth') {
+        style.width = style.maxWidth;
+    }
+
+    if (mode.current === 'width' && props.adjustment !== undefined && props.adjustment !== 0 && style.width !== undefined) {
+        style.width = `calc(${formatwidth(style.width).toString()} ${props.adjustment < 0 ? '-' : '+'} ${Math.abs(props.adjustment).toString()}px)`
+    }
+
+    if (style.cursor === undefined && (props.allowSort ?? true)) {
         style.cursor = 'pointer';
     }
 
-    if (style.position === undefined) {
-        style.position = 'relative';
-    }
-
-    if (style.borderTop === undefined) {
-        style.borderTop = 'none'
-    }
-
     React.useLayoutEffect(() => {
-        if (thref.current == null || props.width !== undefined)
+        if (thref.current == null)
+            return;
+        if (props.adjustment !== undefined && props.adjustment !== 0)
             return;
         const w = GetNodeSize(thref.current)?.width;
+
         if (w === undefined) return;
-            props.setWidth(w, props.MinWidth ?? 100);
+
+        if (mode.current == 'width' && w != props.width) {
+            props.setWidth(w);
+            if (props.minWidth === undefined) {
+                mode.current = 'minWidth';
+                return;
+            }
+            if (props.maxWidth === undefined && style.maxWidth !== undefined) {
+                mode.current = 'maxWidth';
+                return;
+            }
+        }
+        if (mode.current == 'minWidth') {
+            props.setMinWidth(w);
+            if (props.maxWidth === undefined && style.maxWidth !== undefined) {
+                mode.current = 'maxWidth';
+                return;
+            }
+            else {
+                mode.current = 'width';
+                return;
+            }
+                
+        }
+        if (mode.current == 'maxWidth') {
+            props.setMaxWidth(w);
+            if (props.minWidth === undefined)
+                mode.current = 'minWidth';
+            else
+                mode.current = 'width';
+        }
     })
     
-    const onClick = React.useCallback((e) => { 
-        if (props.AllowSort ?? true) props.onSort(props.Key, e, props.Field);
-        e.stopPropagation();
-        }, [props.onSort])
 
-    const onClickBorder = React.useCallback((e) => { 
+    const onClick = React.useCallback((e) => {
+        if (props.allowSort ?? true) props.onSort(e);
+    }, [props.onSort, props.allowSort])
+
+    const onClickBorder = React.useCallback((e) => {
         props.startAdjustment(e);
     }, [props.startAdjustment])
 
+
+    if (props.width != undefined && !props.enabled)
+        return null;
+
     return <th
-                ref={thref}
-                style={style}
-                onClick={onClick}
-                onDrag={(e) => {e.stopPropagation()}}
-            >
-                <div style={{
+        ref={thref}
+        style={style}
+        onClick={onClick}
+        onDrag={(e) => { e.stopPropagation() }}
+    >
+        <div style={{
                 width: 5,
                 height: '100%',
                 position: 'absolute',
@@ -110,20 +146,69 @@ export function AdjColumnHeaderWrapper<T>(props: React.PropsWithChildren<IHeader
                 background: '#e9ecef',
                 cursor: 'col-resize'
             }}
-                    onMouseEnter={onHover}
-                    onMouseLeave={onLeave}
-                    onMouseDown={onClickBorder}
-                ></div>
-            {props.sorted && props.asc ? <div 
-                style={{ position: 'absolute', width: 25 }}>
-                    {SVGIcons.ArrowDropDown} 
-                </div> : null}
-            {props.sorted && !props.asc ? <div 
-                style={{ position: 'absolute', width: 25 }}>
-                    {SVGIcons.ArrowDropUp} 
-                </div> : null}
-            <div style={{
-                marginLeft: (props.sorted ? 25 : 0),
-            }}>{props.children ?? props.Key}</div>
-        </th>
+            onMouseEnter={onHover}
+            onMouseLeave={onLeave}
+            onMouseDown={onClickBorder}
+        ></div>
+        {props.sorted ? <div
+            style={{ position: 'absolute', width: 25 }}>
+            {props.asc ? SVGIcons.ArrowDropUp : SVGIcons.ArrowDropDown}
+        </div> : null}
+        <div style={{
+            marginLeft: (props.sorted ? 25 : 0),
+        }}>{props.children ?? props.colKey}</div>
+    </th>
+}
+
+interface IAdjustableDataWrapperProps extends IDataWrapperProps {
+    adjustment?: number
+}
+
+
+export function AdjustableColumnDataWrapper(props: React.PropsWithChildren<IAdjustableDataWrapperProps>) {
+    const tdref = React.useRef(null);
+    const style = (props.style !== undefined) ? { ...props.style } : {};
+
+    style.overflowX = style.overflowX ?? 'hidden';
+    style.display = style.display ?? 'inline-block'
+    style.width = style.width ?? props.width;
+
+    if (props.dragStart !== undefined) style.cursor = "grab";
+
+    React.useLayoutEffect(() => {
+        if (tdref.current == null)
+            return;
+
+        if (props.adjustment !== undefined && props.adjustment !== 0)
+            return;
+
+        const w = GetNodeSize(tdref.current)?.width;
+        if (w === undefined || w == props.width) return;
+        props.setWidth(w);
+    })
+
+    if (props.adjustment !== undefined && props.adjustment !== 0 && style.width !== undefined)
+        style.width = `calc(${formatwidth(style.width)} ${props.adjustment < 0 ? '-' : '+'} ${Math.abs(props.adjustment).toString()}px)`
+
+    if (props.width != undefined && !props.enabled)
+        return null;
+
+    return (
+        <td
+            ref={tdref}
+            style={style}
+            onClick={props.onClick}
+            draggable={props.dragStart !== undefined}
+            onDragStart={props.dragStart}
+        >
+            {props.children}
+        </td>
+    );
+}
+
+
+function formatwidth(style: string | number) {
+    if (style.toString().endsWith('%') || style.toString().endsWith('px'))
+        return style;
+    return style.toString() + "px"
 }
