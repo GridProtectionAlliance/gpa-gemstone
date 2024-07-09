565d1215d91e9b7d36cec334250cc31325bbf1b0
prestoncraw <prestoncrawford530@gmail.com>
2024-07-09 15:50:54 -0400

    Rework bargroup data and added logic for hover content

--- react-graph/src/Bar/BarGroup.tsx
+++ react-graph/src/Bar/BarGroup.tsx
@@ -23,64 +23,235 @@
 
 import * as React from 'react';
 import Bar, { ContexlessBar } from './Bar';
-import { IGraphContext, GraphContext } from '../GraphContext';
+import { IDataSeries, GraphContext, IGraphContext, AxisIdentifier, AxisMap } from '../GraphContext';
+import Infobox from '../Infobox';
+import { CreateGuid } from '@gpa-gemstone/helper-functions';
+import { ReactTable } from '@gpa-gemstone/react-table';
+
+interface IProps {
+    /**
+     * Flag to determine if infobox is shown when hovering a stacked bar.
+    */
+    ShowHoverInfoBox?: boolean,
+    /**
+     * Background color of the hoverable infobox table. 
+     */
+    HoverColor?: string,
+}
+
+interface IContextData {
+    getPoint?: (xVal: number) => number[] | undefined,
+    enabled: boolean,
+    legendName?: string
+}
+
+interface IHoverData {
+    Name?: string,
+    Value: number
+}
 
 export interface IBarContext extends IGraphContext {
-    GetYPosition?: (timeValue: number, barHeight: number, baseY: number, yValue: number) => number
+    GetYPosition?: (xVal: number, barGuid: string, axis: AxisIdentifier | undefined) => number,
+    AddData: (d: IBarDataSeries, legendName?: string) => string,
+    UpdateData: (key: string, d: IBarDataSeries, legendName?: string) => void,
+}
+
+export interface IBarDataSeries extends IDataSeries {
+    getPoint?: (xVal: number) => number[] | undefined
 }
 
 /**
-    Wraps bar components to vertically stacks bars with matching time values. 
+    Wraps bar components to vertically stacks bars with matching x values. 
 */
-const BarGroup: React.FC = (props) => {
-    const barHeightsRef = React.useRef<Map<number, {positive: number, negative: number}>>(new Map());
+const BarGroup: React.FC<IProps> = (props) => {
     const context = React.useContext(GraphContext);
 
-    const GetYPosition = (timeValue: number, barHeight: number, baseYPosition: number, yValue: number) => {
-        const barHeights = barHeightsRef.current.get(timeValue) ?? {positive: 0, negative: 0};
-        let newYPosition;
-        
-        if (yValue >= 0) {
-            newYPosition = baseYPosition - barHeights.positive - barHeight;
-            barHeights.positive += barHeight;
-        } else {
-            newYPosition = baseYPosition + barHeights.negative;
-            barHeights.negative += barHeight;
+    const guid = React.useRef<string>(CreateGuid());
+    const hoverRef = React.useRef<HTMLDivElement | null>(null);
+    const map = React.useRef<Map<string, IContextData>>(new Map());
+
+    const [barGuids, setBarGuids] = React.useState<string[]>([]);
+    const [hoverData, setHoverData] = React.useState<IHoverData[] | null>(null);
+
+    const GetYPosition = (xVal: number, barGuid: string, axis: AxisIdentifier): number | undefined => {
+        const matchedGUID = barGuids.find(uid => uid === barGuid);
+        if (matchedGUID == null) return;
+
+        const dataSeries = map.current.get(barGuid);
+        if (dataSeries == null || dataSeries.getPoint == null) return;
+        let data: number[] | undefined;
+
+        try {
+            data = dataSeries.getPoint(xVal);
+        } catch {
+            return;
         }
 
-        barHeightsRef.current.set(timeValue, barHeights);
-        return newYPosition;
+        if (data == null) return;
+
+        //Not supporting negative values for now
+        const yValue = data[1]
+        if (yValue < 0) return;
+
+        const baseY = context.YTransformation(0, AxisMap.get(axis));
+        const height = baseY - context.YTransformation(yValue, AxisMap.get(axis))
+        let totalHeight = 0;
+
+        for (let index = 0; index < barGuids.length; index++) {
+            const guid = barGuids[index];
+
+            if (matchedGUID === guid)
+                break;
+
+            const dataSeries = map.current.get(guid);
+            if (dataSeries == null || !dataSeries.enabled || dataSeries?.getPoint == null) continue;
+            let data: number[] | undefined;
+
+            try {
+                data = dataSeries.getPoint(xVal);
+            }
+            catch {
+                continue;
+            }
+
+            if (data == null) continue;
+
+            const yVal = context.YTransformation(data[1], AxisMap.get(axis));
+            const height = baseY - yVal;
+
+            totalHeight += height;
+        }
+
+        const yPosition = baseY - totalHeight - height
+        if (isNaN(yPosition))
+            return;
+
+        return baseY - totalHeight - height
     };
-    
+
+    //Effect to set hoverData
+    React.useEffect(() => {
+        if ((props.ShowHoverInfoBox ?? false) === false) return;
+        if (barGuids.length === 0 || isNaN(context.XHover) || context.XHover > context.XDomain[1] || context.XHover < context.XDomain[0]) {
+            setHoverData(null);
+            return;
+        }
+
+        let points: IHoverData[] = [];
+
+        barGuids.forEach(guid => {
+            const contextData = map.current.get(guid);
+            if (contextData == null || contextData?.getPoint == null) return;
+            let point: number[] | undefined;
+            try {
+                point = contextData.getPoint(context.XHover);
+            }
+            catch {
+                setHoverData(null);
+            }
+
+            if (point == null) return;
+            points.push({ Name: contextData.legendName, Value: point[1] })
+        })
+
+        if (points.length === 0) {
+            setHoverData(null);
+            return
+        }
+
+        setHoverData(points)
+    }, [context.XHover, context.YHover])
+
+    const hoverContent = React.useMemo(() => {
+        if (hoverData == null || hoverData.length === 0) return <></>
+        return (
+            <div ref={hoverRef} style={{ backgroundColor: props.HoverColor ?? 'lightgray', opacity: 0.75 }}>
+                <ReactTable.Table<IHoverData>
+                    Data={hoverData}
+                    SortKey=''
+                    Ascending={false}
+                    OnSort={() => {/* nothing */ }}
+                    KeySelector={row => row.Name ?? row.Value}
+                    RowStyle={{ whiteSpace: 'nowrap' }}
+                >
+                    <ReactTable.Column<IHoverData>
+                        Key={`Name`}
+                        Field={'Name'}
+                        AllowSort={false}
+                    >
+                        {'\u200B'}
+                    </ReactTable.Column>
+                    <ReactTable.Column<IHoverData>
+                        Key={`Value`}
+                        Field={`Value`}
+                        AllowSort={false}
+                        RowStyle={{ textAlign: 'right' }}
+                    >
+                        {'\u200B'}
+                    </ReactTable.Column>
+                </ReactTable.Table>
+            </div>
+        )
+    }, [hoverData])
+
+    const addData = (d: IBarDataSeries, legendName?: string): string => {
+        const guid = context.AddData(d);
+        map.current.set(guid, { enabled: d.enabled, legendName, getPoint: d.getPoint });
+        setBarGuids(guids => [...guids, guid])
+        return guid;
+    }
+
+    const updateData = (guid: string, d: IBarDataSeries, legendName?: string) => {
+        context.UpdateData(guid, d);
+        map.current.set(guid, { enabled: d.enabled, legendName, getPoint: d.getPoint })
+    }
+
     const barContext = React.useMemo(() => {
         return {
             ...context,
-            GetYPosition: GetYPosition
+            GetYPosition: GetYPosition,
+            AddData: addData,
+            UpdateData: updateData
         } as IBarContext;
-    }, [context, GetYPosition]);
-
-    // Reset the bar heights map on each render
-    React.useEffect(() => {
-        barHeightsRef.current.clear();
-    });
+    }, [context]);
 
     return (
         <g>
             {React.Children.map(props.children, (element) => {
-            if (!React.isValidElement(element)) return null;
+                if (!React.isValidElement(element)) return null;
 
-            if (element.type === Bar) {
-                return (
-                    <ContexlessBar
-                        key={element.key}
-                        BarProps={element.props}
-                        Context={barContext}
-                    />
-                );
-            }
-        })}
+                if (element.type === Bar) {
+                    return (
+                        <ContexlessBar
+                            key={element.key}
+                            BarProps={element.props}
+                            Context={barContext}
+                        />
+                    );
+                }
+            })}
+            {hoverData != null ?
+                <Infobox ChildID={guid.current} X={context.XHover} Y={getYHoverPosition(hoverRef.current, context)} Origin={getOrigin(hoverRef.current, context)} Offset={5}>
+                    <div id={guid.current}>{hoverContent}</div>
+                </Infobox>
+                : null}
         </g>
     );
+}
+
+const getOrigin = (hoverRef: HTMLDivElement | null,context: IBarContext) => {
+    if(hoverRef == null) return 'upper-left';
+
+    const middleOfXDomain = (context.XDomain[0] + context.XDomain[1]) / 2
+    if(context.XHover > middleOfXDomain)
+        return 'upper-right'
+    return 'upper-left'
+}
+
+const getYHoverPosition = (hoverRef: HTMLDivElement | null, context: IBarContext): number => {
+    if(hoverRef == null) return context.YHover[0];
+    const middleOfPlot = (context.YDomain[0][0] + context.YDomain [0][1]) / 2;
+    return middleOfPlot
 }
 
 export default BarGroup;
