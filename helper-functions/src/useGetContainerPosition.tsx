725008c9388df32da141cd2aa66b1b04202c2b35
prestoncraw <prestoncrawford530@gmail.com>
2024-10-01 12:06:43 -0400

    Lint fix

--- helper-functions/src/useGetContainerPosition.tsx
+++ helper-functions/src/useGetContainerPosition.tsx
@@ -24,7 +24,7 @@ import * as React from 'react'
 import * as _ from 'lodash';
 
 export const useGetContainerPosition = (containerRef: React.MutableRefObject<HTMLDivElement | null>) => {
-    const [containerPosition, setContainerSize] = React.useState<DOMRect>({ top: 0, left: 0, height: 0, width: 0, x: 0, y: 0, bottom: 0, right: 0, toJSON: () => {} });
+    const [containerPosition, setContainerSize] = React.useState<DOMRect>({ top: 0, left: 0, height: 0, width: 0, x: 0, y: 0, bottom: 0, right: 0, toJSON: () => {/*do nothing */} });
 
     React.useLayoutEffect(() => {
         if (containerRef.current == null) return
