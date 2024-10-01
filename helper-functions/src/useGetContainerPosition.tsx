3f14ed6f90dfc63f0696d87e7ca6faa9227a7795
prestoncraw <prestoncrawford530@gmail.com>
2024-10-01 15:03:39 -0400

    Added annotated comments and change return type for better peformance

--- helper-functions/src/useGetContainerPosition.tsx
+++ helper-functions/src/useGetContainerPosition.tsx
@@ -21,10 +21,30 @@
 //
 // ******************************************************************************************************
 import * as React from 'react'
-import * as _ from 'lodash';
+
+/**
+ * 
+ * @param {React.MutableRefObject<HTMLDivElement | null>} containerRef - A mutable reference object to the container element.
+ * @returns {{ top: number, left: number, height: number, width: number, x: number, y: number, bottom: number, right: number }} - 
+ * - `top`: Distance from the top of the viewport to the top of the container.
+ * - `left`: Distance from the left of the viewport to the left of the container.
+ * - `height`: Height of the container.
+ * - `width`: Width of the container.
+ * - `x`: Horizontal position of the container relative to the viewport.
+ * - `y`: Vertical position of the container relative to the viewport.
+ * - `bottom`: Distance from the top of the viewport to the bottom of the container.
+ * - `right`: Distance from the left of the viewport to the right of the container.
+ */
 
 export const useGetContainerPosition = (containerRef: React.MutableRefObject<HTMLDivElement | null>) => {
-    const [containerPosition, setContainerSize] = React.useState<DOMRect>({ top: 0, left: 0, height: 0, width: 0, x: 0, y: 0, bottom: 0, right: 0, toJSON: () => {/*do nothing */} });
+    const [top, setTop] = React.useState<number>(0);
+    const [left, setLeft] = React.useState<number>(0);
+    const [height, setHeight] = React.useState<number>(0);
+    const [width, setWidth] = React.useState<number>(0);
+    const [x, setX] = React.useState<number>(0);
+    const [y, setY] = React.useState<number>(0);
+    const [bottom, setBottom] = React.useState<number>(0);
+    const [right, setRight] = React.useState<number>(0);
 
     React.useLayoutEffect(() => {
         if (containerRef.current == null) return
@@ -32,9 +52,14 @@ export const useGetContainerPosition = (containerRef: React.MutableRefObject<HTM
         const handleResize = () => {
             if (containerRef.current == null) return
             const newSize = containerRef.current.getBoundingClientRect()
-            if (!_.isEqual(newSize, containerPosition)) {
-                setContainerSize(newSize)
-            }
+            if (newSize.top !== top) setTop(newSize.top);
+            if (newSize.left !== left) setLeft(newSize.left);
+            if (newSize.height !== height) setHeight(newSize.height);
+            if (newSize.width !== width) setWidth(newSize.width);
+            if (newSize.x !== x) setX(newSize.x);
+            if (newSize.y !== y) setY(newSize.y);
+            if (newSize.bottom !== bottom) setBottom(newSize.bottom);
+            if (newSize.right !== right) setRight(newSize.right);
         }
 
         handleResize()
@@ -45,5 +70,5 @@ export const useGetContainerPosition = (containerRef: React.MutableRefObject<HTM
         }
     })
 
-    return containerPosition
+    return {top, left, height, width, x, y, bottom, right}
 }
