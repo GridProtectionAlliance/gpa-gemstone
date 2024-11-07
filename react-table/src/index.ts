e400d2d1708fbcddd8f4bf5fa02758507998dad6
Christoph Lackner <clackner@gridprotectionalliance.org>
2024-11-07 11:01:15 -0500

    Removed Legacy Tables

--- react-table/src/index.ts
+++ react-table/src/index.ts
@@ -23,10 +23,6 @@
 //
 //  ******************************************************************************************************
 
-import Table, {TableProps,Rows, Column} from './Table';
-import {SelectTable, ISelectTableProps} from './SelectTable';
-import { SearchableTable } from './SearchableTable';
-import { DynamicTableProps, DynamicTable } from './DynamicTable';
 import Paging from './Paging';
 import AdjustableTable from './AdjustableTable/Table';
 import { Column as RegularColumn, AdjustableColumn } from './AdjustableTable/Column';
@@ -39,7 +35,5 @@ const ReactTable = {
 
 export {
 	ReactTable,
-	TableProps, SelectTable, ISelectTableProps, SearchableTable, DynamicTable, DynamicTableProps , Rows, Column, Paging
+	Paging
 }
-
-export default Table;
