435fedb703b0241edacf4c8084485a4a8ead7e28
prestoncraw <prestoncrawford530@gmail.com>
2025-02-24 09:40:24 -0500

    Added prop to optionally render a user defined progress bar

--- common-pages/src/BulkUpload.tsx
+++ common-pages/src/BulkUpload.tsx
@@ -43,7 +43,7 @@ interface IProps<T> {
     * */
     ReviewUI: (props: { Data: T[] }) => JSX.Element
     /** 
-    * React Component to be used in the Review Step
+    * React Component to be used in the Complete Step
     * @type {React.FC<IReviewProps<T>>}
     * */
     CompleteUI?: JSX.Element
@@ -72,6 +72,11 @@ interface IProps<T> {
      * @param {string[]} errors - Array of error messages.
      */
     SetErrors: (errors: string[]) => void;
+    /**
+     * Optional Progress Bar component to replace internal Progress Bar. 
+     * @type {JSX.Element} 
+     */
+    ProgressBar?: JSX.Element,
 }
 const steps = [{ short: 'Upload', long: 'Upload', id: 'Upload' }, { short: 'Process', long: 'Process', id: 'Process' }, { short: "Review", id: 'Review', long: 'Review' }, { short: 'Complete', long: 'Complete', id: 'Complete' }]
 const fileExtRegex = /(\.[^.]+)$/;
@@ -97,6 +102,7 @@ export default function BulkUpload<T>(props: IProps<T>) {
     }, [props.Step, currentPipelineIndex, rawFileContent, props.CurrentPipelineStep, props.Pipelines])
 
     const progressSteps = React.useMemo(() => {
+        if(props.ProgressBar != null) return []
         if (currentPipelineIndex == null || currentPipelineIndex > props.Pipelines.length - 1 || props.CurrentPipelineStep > props.Pipelines?.[currentPipelineIndex]?.Steps?.length - 1) return steps
 
         const pipelineSteps = props.Pipelines[currentPipelineIndex].Steps.map((step, i) => ({ short: step.Label, long: step.Label, id: i }))
@@ -177,7 +183,9 @@ export default function BulkUpload<T>(props: IProps<T>) {
                 <div className='col-12 d-flex flex-column h-100'>
                     <div className='row'>
                         <div className='col-12'>
+                            {props.ProgressBar != null ? props.ProgressBar :
                             <ProgressBar steps={progressSteps} activeStep={activeProgressStep} />
+                            }
                         </div>
                     </div>
                     {props.Step === 'Upload' ?
