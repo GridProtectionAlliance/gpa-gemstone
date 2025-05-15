478584fc0fa0f3d7155833fc0ef01d502cf6b0e9
collins-self <cself@GridProtectionAlliance.org>
2025-05-15 10:47:22 -0400

    Add basic table tests
    This commit adds two test to ensure rendering is using supplied prop classes and children table names

--- test/src/__tests__/react-table/Table.test.ts
+++ test/src/__tests__/react-table/Table.test.ts
@@ -1,24 +1,47 @@
-import { afterAll, beforeAll, expect, describe, it } from "@jest/globals";
+import { afterEach, beforeEach, expect, describe, it } from "@jest/globals";
 import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import 'selenium-webdriver/chrome';
 import 'chromedriver';
 
-const rootURL = 'http://localhost:8080';
+const rootURL = 'http://localhost:9000'; // TODO: CHANGE THIS
 let driver: WebDriver;
 const componentTestID = 'table-test-id';
 
 // Before each test, create a selenium webdriver that goes to the rootURL
-beforeAll(async () => {
+beforeEach(async () => {
     driver = await new Builder().forBrowser('chrome').build();
+    await driver.manage().window().setSize(750, 780);
     await driver.get(rootURL); // Navigate to the page
-    await driver.wait(until.titleIs('GPA Test'), 10000); // Wait until the page title is loaded
+    await driver.wait(until.elementIsVisible(driver.findElement(By.css(`#${componentTestID} table`))), 25000);
 });
 
 // close the driver after each test
-afterAll(async () => {
+afterEach(async () => {
     if (driver) await driver.quit();
 });
 
 describe('Table Component', () => {
-    
+    const tableSelector = `#${componentTestID} table`;
+
+    it('Renders the table with proper styles', async () => {
+        const body = await driver.findElement(By.css(`${tableSelector} tbody`));
+        expect(await body.getCssValue('font-style')).toBe('italic');
+
+        const head = await driver.findElement(By.css(`${tableSelector} thead`));
+        expect(await head.getCssValue('font-weight')).toBe('100');
+
+        const row = await driver.findElement(By.css(`${tableSelector} tbody tr`));
+        expect(await row.getCssValue('font-weight')).toBe('700');
+    })
+
+    it('Renders the table with proper column titles', async () => {
+        const tableCols = await driver.findElements(By.css(`thead tr th`));
+        expect(tableCols).toHaveLength(4);
+
+        const colTitles = ['Title', 'Author', 'Vol.', 'Category'];
+        for (let i = 0; i < tableCols.length; i++) {
+            const col = await tableCols[i].getText();
+            expect(col).toBe(colTitles[i]);
+        }
+    })
 });
