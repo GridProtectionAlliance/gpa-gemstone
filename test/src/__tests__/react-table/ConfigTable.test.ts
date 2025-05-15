31bfa3f7d8ae35ea4536eb4faa4e4e967276f435
collins-self <cself@GridProtectionAlliance.org>
2025-05-15 16:07:16 -0400

    Make App a grid and implement basic ConfigurableTable tests

---
+++ test/src/__tests__/react-table/ConfigTable.test.ts
@@ -0,0 +1,96 @@
+//******************************************************************************************************
+//  ConfigTable.test.ts - Gbtc
+//
+//  Copyright (c) 2025, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/05/2025 - Collins Self
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+import { afterEach, beforeEach, expect, describe, it } from "@jest/globals";
+import { Builder, By, until, WebDriver } from 'selenium-webdriver';
+import 'selenium-webdriver/chrome';
+import 'chromedriver';
+
+const rootURL = `http://localhost:${global.PORT}`;
+let driver: WebDriver;
+const componentTestID = 'configtable-test-id';
+
+// Before each test, create a selenium webdriver that goes to the rootURL
+beforeEach(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.manage().window().setSize(1600, 750);
+    await driver.get(rootURL); // Navigate to the page
+    await driver.wait(until.elementIsVisible(driver.findElement(By.css(`#${componentTestID}-1 table`))), 25000);
+    await driver.wait(until.elementIsVisible(driver.findElement(By.css(`#${componentTestID}-2 table`))), 25000);
+});
+
+// close the driver after each test
+afterEach(async () => {
+    if (driver) await driver.quit();
+});
+
+describe('ConfigTable Component 1', () => {
+    const tableSelector = `#${componentTestID}-1 table`;
+
+    it('Renders the table with proper styles', async () => {
+        const body = await driver.findElement(By.css(`${tableSelector} tbody`));
+        expect(await body.getCssValue('font-style')).toBe('italic');
+
+        const head = await driver.findElement(By.css(`${tableSelector} thead`));
+        expect(await head.getCssValue('font-weight')).toBe('100');
+
+        const row = await driver.findElement(By.css(`${tableSelector} tbody tr`));
+        expect(await row.getCssValue('font-weight')).toBe('700');
+    })
+
+    it('Renders the table with proper, default column titles', async () => {
+        const tableCols = await driver.findElements(By.css(`${tableSelector} thead tr th`));
+        expect(tableCols).toHaveLength(4);
+
+        const colTitles = ['Title', 'Author', 'Vol.', 'Category'];
+        for (let i = 0; i < tableCols.length; i++) {
+            const col = await tableCols[i].getText();
+            expect(col).toBe(colTitles[i]);
+        }
+    })
+});
+
+describe('ConfigTable Component 2: Children Load Columns', () => {
+    const tableSelector = `#${componentTestID}-2 table`;
+
+    it('Renders the table with proper styles', async () => {
+        const body = await driver.findElement(By.css(`${tableSelector} tbody`));
+        expect(await body.getCssValue('font-style')).toBe('italic');
+
+        const head = await driver.findElement(By.css(`${tableSelector} thead`));
+        expect(await head.getCssValue('font-weight')).toBe('100');
+
+        const row = await driver.findElement(By.css(`${tableSelector} tbody tr`));
+        expect(await row.getCssValue('font-weight')).toBe('700');
+    })
+
+    it('Renders the table with proper, default column titles', async () => {
+        const tableCols = await driver.findElements(By.css(`${tableSelector} thead tr th`));
+        expect(tableCols).toHaveLength(4);
+
+        const colTitles = ['Title', 'Author', 'Vol.', 'Category'];
+        for (let i = 0; i < tableCols.length; i++) {
+            const col = await tableCols[i].getText();
+            expect(col).toBe(colTitles[i]);
+        }
+    })
+});
---
+++ test/src/components/react-table/ConfigurableTable.tsx
@@ -0,0 +1,172 @@
+//******************************************************************************************************
+//  ConfigurableTable.tsx - Gbtc
+//
+//  Copyright (c) 2025, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  05/14/2025 - Collins Self
+//       Generated original code.
+//
+//******************************************************************************************************
+
+import { ConfigurableTable, ConfigurableColumn, Column } from "@gpa-gemstone/react-table";
+import tableData from '../../test-data/table_test_data.json';
+import React from "react";
+import _ from "lodash";
+
+type Book = {
+    Title: string;
+    Author: string;
+    Volume: number;
+    Category: string;
+};
+
+const ConfigurableTableTestComponent: React.FC<{ ComponentTestID: string }> = (props) => {
+    const [data, setData] = React.useState<Book[]>([]);
+    const [sortKey, setSortKey] = React.useState<keyof Book>('Title');
+    const [asc, setAsc] = React.useState<boolean>(true);
+    const [cols, setCols] = React.useState<string[]>([]);
+
+    React.useEffect(() => {
+        setData(tableData.slice());
+    }, []);
+
+    React.useEffect(() => {
+        if (data.length == 0)
+            return;
+        const updatedCols = Object.keys(data[0]);
+        if (!_.isEqual(updatedCols, cols))
+            setCols(updatedCols);
+    }, [data]);
+
+    const headerHeight = 45;
+    const bodyHeight = 300;
+    const containerHeight = bodyHeight + headerHeight + 1;
+    const containerWidth = 700;
+
+    return (<>
+        <div id={props.ComponentTestID + "-1"} className="border col p-0 m-0" style={{ maxHeight: `${containerHeight}px`, maxWidth: `${containerWidth}px` }}>
+            <ConfigurableTable<Book>
+                TableClass={`table table-hover`}
+                RowStyle={{ fontSize: 'smaller', fontWeight: 'bolder' }}
+                TheadStyle={{ fontSize: 'smaller', fontWeight: 'lighter', maxHeight: `${headerHeight}px` }}
+                TbodyStyle={{ fontStyle: 'italic', maxHeight: `${bodyHeight}px` }}
+                TfootStyle={{ fontStyle: 'bold' }}
+                Data={data}
+                SortKey={sortKey}
+                Ascending={asc}
+                OnSort={(clickedCol) => {
+                    if (clickedCol.colKey === sortKey)
+                        setAsc(!asc);
+                    else
+                        setSortKey(clickedCol.colKey as keyof Book);
+                }}
+                OnClick={(rowClicked) => {
+                    alert(`${props.ComponentTestID}: ${rowClicked.row.Title}`);
+                }}
+                KeySelector={(d) => `${d.Volume}-${d.Title}`}
+                ModalZIndex={30000}
+                //SettingsPortal={}
+                OnSettingsChange={() => { }}
+                LocalStorageKey={props.ComponentTestID + '-1'}
+            >
+                <ConfigurableColumn Key="Title" Default={true} Label={'Title'}>
+                    <Column<Book>
+                        Key="Title"
+                        AllowSort={true}
+                        Field="Title"
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    >
+                        Title
+                    </Column>
+                </ConfigurableColumn>
+                <ConfigurableColumn Key="Author" Default={true} Label={'Author'}>
+                    <Column<Book>
+                        Key="Author"
+                        AllowSort={true}
+                        Field="Author"
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    >
+                        Author
+                    </Column>
+                </ConfigurableColumn>
+                <ConfigurableColumn Key="Volume" Default={true} Label={'Vol.'}>
+                    <Column<Book>
+                        Key="Volume"
+                        AllowSort={true}
+                        Field="Volume"
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    >
+                        Vol.
+                    </Column>
+                </ConfigurableColumn>
+                <ConfigurableColumn Key="Category" Default={true} Label={'Category'}>
+                    <Column<Book>
+                        Key="Category"
+                        AllowSort={true}
+                        Field="Category"
+                        HeaderStyle={{ width: 'auto' }}
+                        RowStyle={{ width: 'auto' }}
+                    >
+                        Category
+                    </Column>
+                </ConfigurableColumn>
+            </ConfigurableTable>
+        </div>
+        <div id={props.ComponentTestID + "-2"} className="border col p-0 m-0" style={{ maxHeight: `${containerHeight}px`, maxWidth: `${containerWidth}px` }}>
+            <ConfigurableTable<Book>
+            TableClass={`table table-hover`}
+            RowStyle={{ fontSize: 'smaller', fontWeight: 'bolder' }}
+            TheadStyle={{ fontSize: 'smaller', fontWeight: 'lighter', maxHeight: `${headerHeight}px` }}
+            TbodyStyle={{ fontStyle: 'italic', maxHeight: `${bodyHeight}px` }}
+            TfootStyle={{ fontStyle: 'bold' }}
+            Data={data}
+            SortKey={sortKey}
+            Ascending={asc}
+            OnSort={(clickedCol) => {
+                if (clickedCol.colKey === sortKey)
+                    setAsc(!asc);
+                else
+                    setSortKey(clickedCol.colKey as keyof Book);
+            }}
+            OnClick={(rowClicked) => {
+                alert(`${props.ComponentTestID}: ${rowClicked.row.Title}`);
+            }}
+            KeySelector={(d) => `${d.Volume}-${d.Title}`}
+            ModalZIndex={30000}
+            //SettingsPortal={}
+            OnSettingsChange={() => {}}
+            LocalStorageKey={props.ComponentTestID + '-2'}
+            >
+                {cols.map(col =>
+                    <ConfigurableColumn Key={col} Default={true} Label={col} key={col}>
+                        <Column<any> key={col}
+                            Key={col} Field={col}
+                            AllowSort={true} Adjustable={false}
+                            HeaderStyle={{ width: 'auto', minWidth: '20%' }}
+                            RowStyle={{ width: 'auto', minWidth: '20%' }}
+                        >{col}
+                        </Column>
+                    </ConfigurableColumn>
+                )}
+        </ConfigurableTable>
+    </div>
+    </>)
+}
+
+export default ConfigurableTableTestComponent;
