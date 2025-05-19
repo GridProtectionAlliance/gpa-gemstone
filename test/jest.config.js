418aaf544484cb9d828c09a02885f69e6b307489
collins-self <cself@GridProtectionAlliance.org>
2025-05-19 16:13:37 -0400

    remove styles causing window display problem

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
     setupFiles: ['<rootDir>/jest.setup.ts'],
-    testEnvironment: "jsdom",
+    testEnvironment: "node",
     testTimeout: 30000,
     transform: {
         "^.+\.tsx?$": ["ts-jest",{}],
