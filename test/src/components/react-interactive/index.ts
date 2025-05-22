81d048b217bee94a070f5854521e4f0c9c7ab7d8
collins-self <cself@GridProtectionAlliance.org>
2025-05-22 15:07:46 -0400

    update components to export via index.ts

---
+++ test/src/components/react-interactive/index.ts
@@ -0,0 +1,3 @@
+export { default as AlertTestComponent } from './Alert';
+export { default as BreadcrumbTestComponent } from './Breadcrumb';
+export { default as BtnDropdownTestComponent } from './DropdownButton';
