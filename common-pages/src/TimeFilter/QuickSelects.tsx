24a09f76b4525b6cedcdef6b06edb4f0cf615d00
natalie beatty <nbeatty@gridprotectionalliance.org>
2025-12-10 11:10:26 -0500

    add "Last 45 Seconds" QuickSelect for datetime-short format

--- common-pages/src/TimeFilter/QuickSelects.tsx
+++ common-pages/src/TimeFilter/QuickSelects.tsx
@@ -146,6 +146,21 @@ const getColSize = (dateTimeSetting: DateTimeSetting, splitSelects: boolean) => 
 export const AvailableQuickSelects: IQuickSelect[] = [
     {
     {
+        label: 'Last 45 Seconds', createFilter: (tz, format) => {
+            const offset = momentTZ.tz(moment.utc().startOf('second').subtract(45, 'seconds').format('YYYY-MM-DDTHH:mm:ss.SSSSS'), tz).utcOffset();
+            const t = moment.utc().add(offset, 'seconds').startOf('second');
+
+            return {
+                end: t.format(getFormat(format)),
+                unit: 's',
+                duration: 45,
+            }
+        },
+        hideQuickPick: (f) => {
+            return f !== 'datetime-short'
+        }
+    },
+    {
         label: 'Last 1 Minute', createFilter: (tz, format) => {
             const offset = momentTZ.tz(moment.utc().startOf('minute').subtract(1, 'minute').format('YYYY-MM-DDTHH:mm:ss.SSSSS'), tz).utcOffset();
             const t = moment.utc().add(offset, 'minutes').startOf('minute');
