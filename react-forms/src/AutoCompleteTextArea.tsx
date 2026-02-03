3ad4af6f202eff3dbd0a03d0e90d8dc1bbd3b8fa
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-02-03 11:08:46 -0500

    fix handleOptionClick calls

--- react-forms/src/AutoCompleteTextArea.tsx
+++ react-forms/src/AutoCompleteTextArea.tsx
@@ -127,7 +127,7 @@ export default function AutoCompleteTextArea<T>(props: IProps<T>) {
               <tbody>
                 {autoCompleteOptions.map((f, i) => (
                   f.Value === props.Record[props.Field] ? null :
-                  <tr key={i} onMouseDown={(evt) => handleOptionClick(evt, f)}>
+                  <tr key={i} onMouseDown={(_) => handleOptionClick(f)}>
                     <td>
                     {f.Label}
                     </td>
