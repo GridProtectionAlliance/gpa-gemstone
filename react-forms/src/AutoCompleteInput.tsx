8f3c88e890b88597ae327ff7443478983f86d7c7
natalie beatty <nbeatty@gridprotectionalliance.org>
2026-02-03 10:46:53 -0500

    formatting

--- react-forms/src/AutoCompleteInput.tsx
+++ react-forms/src/AutoCompleteInput.tsx
@@ -65,153 +65,152 @@ interface IProps<T> extends Gemstone.TSX.Interfaces.IBaseFormProps<T> {
   */
   DefaultValue?: number
   /**
-   * Values for AutoCompletion
-   * @type {string}
-   */
+    *Values for AutoCompletion
+    * @type {string}
+  */
   Options: string[]
 }
 
 export default function AutoCompleteInput<T>(props: IProps<T>) {
-    const autoCompleteInput = React.useRef<HTMLDivElement>(null);
-    const tableContainer = React.useRef<HTMLDivElement>(null);
-    const selectTable = React.useRef<HTMLTableElement>(null);
-    const [show, setShow] = React.useState<boolean>(false);
-    const [autoCompleteOptions, setAutoCompleteOptions] = React.useState<Gemstone.TSX.Interfaces.ILabelValue<string>[]>([])
-    const [position, setPosition] = React.useState<Gemstone.TSX.Interfaces.IElementPosition>({ Top: 0, Left: 0, Width: 0, Height: 0 });
+  const autoCompleteInput = React.useRef<HTMLDivElement>(null);
+  const tableContainer = React.useRef<HTMLDivElement>(null);
+  const selectTable = React.useRef<HTMLTableElement>(null);
+  const [show, setShow] = React.useState<boolean>(false);
+  const [autoCompleteOptions, setAutoCompleteOptions] = React.useState<Gemstone.TSX.Interfaces.ILabelValue<string>[]>([])
+  const [position, setPosition] = React.useState<Gemstone.TSX.Interfaces.IElementPosition>({ Top: 0, Left: 0, Width: 0, Height: 0 });
     
-    const handleOptionClick = (option: Gemstone.TSX.Interfaces.ILabelValue<string>) => {
-        props.Record[props.Field] = option.Value as any;
-        props.Setter(props.Record)
-        setShow(false)
+  const handleOptionClick = (option: Gemstone.TSX.Interfaces.ILabelValue<string>) => {
+    props.Record[props.Field] = option.Value as any;
+    props.Setter(props.Record)
+    setShow(false)
+  }
+  React.useEffect(() => {
+    if (autoCompleteOptions != null && autoCompleteOptions.length !== 0) {
+      setShow(true);
+      return
     }
-    React.useEffect(() => {
-        if (autoCompleteOptions != null && autoCompleteOptions.length !== 0) {
-            setShow(true);
-            return
-        }
-        setShow(false);
-    }, [autoCompleteOptions])
+    setShow(false);
+  }, [autoCompleteOptions])
     
-    React.useEffect(() => {
-        const rawValue = props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString();
-        handleAutoComplete(rawValue, props.Options, setAutoCompleteOptions);
-    }, [props.Record[props.Field]])
+  React.useEffect(() => {
+    const rawValue = props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString();
+    handleAutoComplete(rawValue, props.Options, setAutoCompleteOptions);
+  }, [props.Record[props.Field]])
 
-    React.useLayoutEffect(() => {
-     const updatePosition = _.debounce(() => {
-      if (autoCompleteInput.current != null) {
-        const rect = autoCompleteInput.current.getBoundingClientRect();
-        setPosition({ Top: rect.bottom, Left: rect.left, Width: rect.width, Height: rect.height });
+  React.useLayoutEffect(() => {
+    const updatePosition = _.debounce(() => {
+    if (autoCompleteInput.current != null) {
+      const rect = autoCompleteInput.current.getBoundingClientRect();
+      setPosition({ Top: rect.bottom, Left: rect.left, Width: rect.width, Height: rect.height });
       }
     }, 200);
 
     const handleScroll = () => {
       if (tableContainer.current == null) return
       updatePosition()
-    };
+      };
 
-      updatePosition();
+    updatePosition();
 
-      window.addEventListener('scroll', handleScroll, true);
-      window.addEventListener('resize', updatePosition);
+    window.addEventListener('scroll', handleScroll, true);
+    window.addEventListener('resize', updatePosition);
 
-      return () => {
-        window.removeEventListener('scroll', handleScroll, true);
-        window.removeEventListener('resize', updatePosition);
-        updatePosition.cancel();
-     
+    return () => {
+      window.removeEventListener('scroll', handleScroll, true);
+      window.removeEventListener('resize', updatePosition);
+      updatePosition.cancel();
     }
-
   }, [autoCompleteOptions]);
 
 
-    return (
-        <div ref={autoCompleteInput}>
-
-            <Input
-                Valid={props.Valid}
-                Record={props.Record}
-                Setter={props.Setter}
-                Field={props.Field}
-                Feedback={props.Feedback}
-                Style={props.Style}
-                AllowNull={props.AllowNull}
-                Size={props.Size}
-                DefaultValue={props.DefaultValue}
-            />
-            {!show ? null :
-                <Portal>
-                <div ref={tableContainer} className='popover'
-                style={{
-                    maxHeight: window.innerHeight - position.Top,
-                    overflowY: 'auto',
-                    padding: '10 5',
-                    display: show ? 'block' : 'none',
-                    position: 'absolute',
-                    zIndex: 9999,
-                    top: `${position.Top}px`,
-                    left: `${position.Left}px`,
-                    minWidth: `${position.Width}px`,
-                    maxWidth: '100%'
-                }}
-                >
-                <table className="table table-hover" style={{ margin: 0 }} ref={selectTable}>
-                    <tbody>
-                    {autoCompleteOptions.map((f, i) => (
-                        f.Value === props.Record[props.Field] ? null :
-                        <tr key={i} onMouseDown={(evt) => handleOptionClick(evt, f)}>
-                            <td>
-                            {f.Label}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                </div>
-            </Portal> 
-            }
-        </div>
+  return (
+    <div ref={autoCompleteInput}>
+      <Input
+        Valid={props.Valid}
+        Record={props.Record}
+        Setter={props.Setter}
+        Field={props.Field}
+        Feedback={props.Feedback}
+        Style={props.Style}
+        AllowNull={props.AllowNull}
+        Size={props.Size}
+        DefaultValue={props.DefaultValue}
+      />
+      {!show ? null :
+        <Portal>
+          <div ref={tableContainer} className='popover'
+            style={{
+              maxHeight: window.innerHeight - position.Top,
+              overflowY: 'auto',
+              padding: '10 5',
+              display: show ? 'block' : 'none',
+              position: 'absolute',
+              zIndex: 9999,
+              top: `${position.Top}px`,
+              left: `${position.Left}px`,
+              minWidth: `${position.Width}px`,
+              maxWidth: '100%'
+            }}
+        >
+            <table className="table table-hover" style={{ margin: 0 }} ref={selectTable}>
+              <tbody>
+                {autoCompleteOptions.map((f, i) => (
+                  f.Value === props.Record[props.Field] ? null :
+                    <tr key={i} onMouseDown={(evt) => handleOptionClick(evt, f)}>
+                      <td>
+                        {f.Label}
+                      </td>
+                    </tr>
+                    )
+                  )
+                  }
+              </tbody>
+            </table>
+          </div>
+        </Portal> 
+        }
+      </div>
     )
 }
 
 export const handleAutoComplete = (inputString: string, autoCompletes: string[], autoCompleteSetter: React.Dispatch<React.SetStateAction<Gemstone.TSX.Interfaces.ILabelValue<string>[]>>) => {
 
-    // Find all variables in the searchString
-    const regex = /\{([^\s{}]*)/g;
-    let match: RegExpExecArray | null;
-    const variables: { name: string; start: number; end: number; hasClosingBrace: boolean }[] = [];
+  // Find all variables in the searchString
+  const regex = /\{([^\s{}]*)/g;
+  let match: RegExpExecArray | null;
+  const variables: { name: string; start: number; end: number; hasClosingBrace: boolean }[] = [];
 
-    while ((match = regex.exec(inputString)) !== null) {
-            const variableName = match[1];
-            const start = match.index;
-            let end = regex.lastIndex;
+  while ((match = regex.exec(inputString)) !== null) {
+    const variableName = match[1];
+    const start = match.index;
+    let end = regex.lastIndex;
 
-            // Check if there is a closing '}'
-            let hasClosingBrace = false;
-            if (inputString[end] === '}') {
-                hasClosingBrace = true;
-                end++; // Include the closing '}'
-            }
+    // Check if there is a closing '}'
+    let hasClosingBrace = false;
+    if (inputString[end] === '}') {
+      hasClosingBrace = true;
+      end++; // Include the closing '}'
+    }
 
-            variables.push({ name: variableName, start, end, hasClosingBrace });
-        } // Find the first invalid variable
-        const invalidVariable = variables.find((v) => !v.hasClosingBrace || !autoCompletes.some((cv) => cv.toLowerCase() === v.name.toLowerCase()));
+    variables.push({ name: variableName, start, end, hasClosingBrace });
+  } // Find the first invalid variable
+  const invalidVariable = variables.find((v) => !v.hasClosingBrace || !autoCompletes.some((cv) => cv.toLowerCase() === v.name.toLowerCase()));
 
-        if (invalidVariable == null) {
-            return([]);
-        }
+  if (invalidVariable == null) {
+    return([]);
+  }
 
-        // Find suggestions for the invalid variable
-        const possibleVariables = autoCompletes.filter(v => v.toLowerCase().includes(invalidVariable.name.toLowerCase()));
+  // Find suggestions for the invalid variable
+  const possibleVariables = autoCompletes.filter(v => v.toLowerCase().includes(invalidVariable.name.toLowerCase()));
 
-        // Generate suggestions by replacing the FIRST invalid variable
-        const suggestions = possibleVariables.map((pv) => {
-            const before = inputString.substring(0, invalidVariable.start);
-            const after = inputString.substring(invalidVariable.end);
+  // Generate suggestions by replacing the FIRST invalid variable
+  const suggestions = possibleVariables.map((pv) => {
+  const before = inputString.substring(0, invalidVariable.start);
+  const after = inputString.substring(invalidVariable.end);
 
-            // Ensure we have braces around the variable and add closing '}' if it was missing
-            const variableWithBraces = invalidVariable.hasClosingBrace ? `{${pv}}` : `{${pv}}`;
-            return { Label: `${variableWithBraces}`, Value: `${before}${variableWithBraces}${after}` };
-        });
-        autoCompleteSetter(suggestions);
-    };
+  // Ensure we have braces around the variable and add closing '}' if it was missing
+  const variableWithBraces = invalidVariable.hasClosingBrace ? `{${pv}}` : `{${pv}}`;
+  return { Label: `${variableWithBraces}`, Value: `${before}${variableWithBraces}${after}` };
+  });
+  autoCompleteSetter(suggestions);
+};
--- react-forms/src/AutoCompleteTextArea.tsx
+++ react-forms/src/AutoCompleteTextArea.tsx
@@ -9,214 +9,209 @@ interface IProps<T> extends Gemstone.TSX.Interfaces.IBaseFormProps<T> {
     * Number of rows for the textarea
     * @type {number}
   */
-  Rows: number;
+    Rows: number;
   /**
     * Function to determine the validity of a field
     * @param field - Field of the record to check
     * @returns {boolean}
   */
-  Valid: (field: keyof T) => boolean;
+    Valid: (field: keyof T) => boolean; 
   /**
     * Feedback message to show when input is invalid
     * @type {string}
     * @optional
   */
-  Feedback?: string;
+    Feedback?: string;
   /**
     * Help message or element to display
     * @type {string | JSX.Element}
     * @optional
   */
-  Help?: string | JSX.Element;
+    Help?: string | JSX.Element;
   /**
-   * Autocomplete options
-   * @type {string}
-   */
-  Options: string[]
+    * Autocomplete options
+    * @type {string}
+  */
+    Options: string[]
 }
 
 export default function AutoCompleteTextArea<T>(props: IProps<T>) {
+  const autoCompleteTextArea = React.useRef<HTMLDivElement>(null);
+  const tableContainer = React.useRef<HTMLDivElement>(null);
+  const selectTable = React.useRef<HTMLTableElement>(null);
+  const textAreaElement = React.useRef<HTMLTextAreaElement | null>(null);
+  const [show, setShow] = React.useState<boolean>(false);
+  const [autoCompleteOptions, setAutoCompleteOptions] = React.useState<Gemstone.TSX.Interfaces.ILabelValue<string>[]>([])
+  const [position, setPosition] = React.useState<Gemstone.TSX.Interfaces.IElementPosition>({ Top: 0, Left: 0, Width: 0, Height: 0 });
     
-    const autoCompleteTextArea = React.useRef<HTMLDivElement>(null);
-    const tableContainer = React.useRef<HTMLDivElement>(null);
-    const selectTable = React.useRef<HTMLTableElement>(null);
-    const textAreaElement = React.useRef<HTMLTextAreaElement | null>(null);
-    const [show, setShow] = React.useState<boolean>(false);
-    const [autoCompleteOptions, setAutoCompleteOptions] = React.useState<Gemstone.TSX.Interfaces.ILabelValue<string>[]>([])
-    const [position, setPosition] = React.useState<Gemstone.TSX.Interfaces.IElementPosition>({ Top: 0, Left: 0, Width: 0, Height: 0 });
-    
-    const handleOptionClick = (option: Gemstone.TSX.Interfaces.ILabelValue<string>) => {
-        props.Record[props.Field] = option.Value as any;
-        props.Setter(props.Record)
-        setShow(false)
+  const handleOptionClick = (option: Gemstone.TSX.Interfaces.ILabelValue<string>) => {
+    props.Record[props.Field] = option.Value as any;
+    props.Setter(props.Record)
+    setShow(false)
+  }
+  React.useEffect(() => {
+    if (autoCompleteOptions != null && autoCompleteOptions.length !== 0) {
+      setShow(true);
+      return
     }
-    React.useEffect(() => {
-        if (autoCompleteOptions != null && autoCompleteOptions.length !== 0) {
-            setShow(true);
-            return
-        }
-        setShow(false);
-    }, [autoCompleteOptions])
+    setShow(false);
+  },  [autoCompleteOptions])
     
-    React.useEffect(() => {
-        const rawValue = props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString();
-        handleAutoComplete(rawValue, props.Options, setAutoCompleteOptions);
-    }, [props.Record[props.Field]])
+  React.useEffect(() => {
+    const rawValue = props.Record[props.Field] == null ? '' : (props.Record[props.Field] as any).toString();
+    handleAutoComplete(rawValue, props.Options, setAutoCompleteOptions);
+  },  [props.Record[props.Field]])
 
-    React.useLayoutEffect(() => {
-     const updatePosition = _.debounce(() => {
-      if (autoCompleteTextArea.current != null) {
-        if (textAreaElement.current == null) {
-            const textAreaComponent = autoCompleteTextArea.current.firstChild;
-            if (textAreaComponent == null) {return}
-            textAreaComponent.childNodes.forEach((element) => element.nodeName === 'TEXTAREA' ? textAreaElement.current = element as HTMLTextAreaElement : null)
-        }
-        if (textAreaElement.current == null) {return}
-        const rect = textAreaElement.current.getBoundingClientRect();
-        const [ caret_X, caret_Y ] = getCaretPosition(textAreaElement.current);
-        // we want to offset the position based on the cursor position within the text area.
-        setPosition({ Top: rect.top + caret_Y - rect.bottom, Left: rect.left + caret_X, Width: rect.width, Height: rect.height });
-      }
-    }, 200);
+  React.useLayoutEffect(() => {
+    const updatePosition = _.debounce(() => {
+    if (autoCompleteTextArea.current != null) {
+    if (textAreaElement.current == null) {
+      const textAreaComponent = autoCompleteTextArea.current.firstChild;
+      if (textAreaComponent == null) {return}
+      textAreaComponent.childNodes.forEach((element) => element.nodeName === 'TEXTAREA' ? textAreaElement.current = element as HTMLTextAreaElement : null)
+    }
+    if (textAreaElement.current == null) {return}
+    const rect = textAreaElement.current.getBoundingClientRect();
+    const [ caret_X, caret_Y ] = getCaretPosition(textAreaElement.current);
+    // we want to offset the position based on the cursor position within the text area.
+    setPosition({ Top: rect.top + caret_Y - rect.bottom, Left: rect.left + caret_X, Width: rect.width, Height: rect.height });
+    }
+  }, 200);
 
-    const handleScroll = () => {
-      if (tableContainer.current == null) return
-      updatePosition()
-    };
+  const handleScroll = () => {
+    if (tableContainer.current == null) return
+    updatePosition()
+  };
 
-      updatePosition();
+  updatePosition();
 
-      window.addEventListener('scroll', handleScroll, true);
-      window.addEventListener('resize', updatePosition);
+  window.addEventListener('scroll', handleScroll, true);
+  window.addEventListener('resize', updatePosition);
 
-      return () => {
-        window.removeEventListener('scroll', handleScroll, true);
-        window.removeEventListener('resize', updatePosition);
-        updatePosition.cancel();
-     
+  return () => {
+    window.removeEventListener('scroll', handleScroll, true);
+    window.removeEventListener('resize', updatePosition);
+    updatePosition.cancel();
     }
 
   }, [autoCompleteOptions]);
 
 
-    return (
-        <div ref={autoCompleteTextArea}>
-
-            <TextArea
-                Valid={props.Valid}
-                Record={props.Record}
-                Setter={props.Setter}
-                Field={props.Field}
-                Rows={props.Rows}
-            />
-            {!show ? null :
-                <Portal>
-                <div ref={tableContainer} className='popover'
-                style={{
-                    maxHeight: window.innerHeight - position.Top,
-                    overflowY: 'auto',
-                    padding: '10 5',
-                    display: show ? 'block' : 'none',
-                    position: 'absolute',
-                    zIndex: 9999,
-                    top: `${position.Top}px`,
-                    left: `${position.Left}px`,
-                    minWidth: `${position.Width}px`,
-                    maxWidth: '100%'
-                }}
-                >
-                <table className="table table-hover" style={{ margin: 0 }} ref={selectTable}>
-                    <tbody>
-                    {autoCompleteOptions.map((f, i) => (
-                        f.Value === props.Record[props.Field] ? null :
-                        <tr key={i} onMouseDown={(evt) => handleOptionClick(evt, f)}>
-                            <td>
-                            {f.Label}
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-                </div>
-            </Portal> 
-            }
-        </div>
-    )
+  return (
+    <div ref={autoCompleteTextArea}>
+      <TextArea
+        Valid={props.Valid}
+        Record={props.Record}
+        Setter={props.Setter}
+        Field={props.Field}
+        Rows={props.Rows}
+      />
+        {!show ? null :
+          <Portal>
+          <div ref={tableContainer} className='popover'
+          style={{
+            maxHeight: window.innerHeight - position.Top,
+            overflowY: 'auto',
+            padding: '10 5',
+            display: show ? 'block' : 'none',
+            position: 'absolute',
+            zIndex: 9999,
+            top: `${position.Top}px`,
+            left: `${position.Left}px`,
+            minWidth: `${position.Width}px`,
+            maxWidth: '100%'
+            }}
+          >
+            <table className="table table-hover" style={{ margin: 0 }} ref={selectTable}>
+              <tbody>
+                {autoCompleteOptions.map((f, i) => (
+                  f.Value === props.Record[props.Field] ? null :
+                  <tr key={i} onMouseDown={(evt) => handleOptionClick(evt, f)}>
+                    <td>
+                    {f.Label}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          </Portal> 
+        }
+    </div>
+  )
 }
 
 export const handleAutoComplete = (inputString: string, autoCompletes: string[], autoCompleteSetter: React.Dispatch<React.SetStateAction<Gemstone.TSX.Interfaces.ILabelValue<string>[]>>) => {
+  // Find all variables in the searchString
+  const regex = /\{([^\s{}]*)/g;
+  let match: RegExpExecArray | null;
+  const variables: { name: string; start: number; end: number; hasClosingBrace: boolean }[] = [];
 
-    // Find all variables in the searchString
-    const regex = /\{([^\s{}]*)/g;
-    let match: RegExpExecArray | null;
-    const variables: { name: string; start: number; end: number; hasClosingBrace: boolean }[] = [];
+  while ((match = regex.exec(inputString)) !== null) {
+    const variableName = match[1];
+    const start = match.index;
+    let end = regex.lastIndex;
 
-    while ((match = regex.exec(inputString)) !== null) {
-            const variableName = match[1];
-            const start = match.index;
-            let end = regex.lastIndex;
+    // Check if there is a closing '}'
+    let hasClosingBrace = false;
+    if (inputString[end] === '}') {
+      hasClosingBrace = true;
+      end++; // Include the closing '}'
+    }
 
-            // Check if there is a closing '}'
-            let hasClosingBrace = false;
-            if (inputString[end] === '}') {
-                hasClosingBrace = true;
-                end++; // Include the closing '}'
-            }
+    variables.push({ name: variableName, start, end, hasClosingBrace });
+  } // Find the first invalid variable
+  const invalidVariable = variables.find((v) => !v.hasClosingBrace || !autoCompletes.some((cv) => cv.toLowerCase() === v.name.toLowerCase()));
+    if (invalidVariable == null) {
+      return([]);
+    }
 
-            variables.push({ name: variableName, start, end, hasClosingBrace });
-        } // Find the first invalid variable
-        const invalidVariable = variables.find((v) => !v.hasClosingBrace || !autoCompletes.some((cv) => cv.toLowerCase() === v.name.toLowerCase()));
+  // Find suggestions for the invalid variable
+  const possibleVariables = autoCompletes.filter(v => v.toLowerCase().includes(invalidVariable.name.toLowerCase()));
 
-        if (invalidVariable == null) {
-            return([]);
-        }
+  // Generate suggestions by replacing the FIRST invalid variable
+  const suggestions = possibleVariables.map((pv) => {
+  const before = inputString.substring(0, invalidVariable.start);
+  const after = inputString.substring(invalidVariable.end);
 
-        // Find suggestions for the invalid variable
-        const possibleVariables = autoCompletes.filter(v => v.toLowerCase().includes(invalidVariable.name.toLowerCase()));
-
-        // Generate suggestions by replacing the FIRST invalid variable
-        const suggestions = possibleVariables.map((pv) => {
-            const before = inputString.substring(0, invalidVariable.start);
-            const after = inputString.substring(invalidVariable.end);
-
-            // Ensure we have braces around the variable and add closing '}' if it was missing
-            const variableWithBraces = invalidVariable.hasClosingBrace ? `{${pv}}` : `{${pv}}`;
-            return { Label: `${variableWithBraces}`, Value: `${before}${variableWithBraces}${after}` };
-        });
-        autoCompleteSetter(suggestions);
-    };
+  // Ensure we have braces around the variable and add closing '}' if it was missing
+  const variableWithBraces = invalidVariable.hasClosingBrace ? `{${pv}}` : `{${pv}}`;
+  return { Label: `${variableWithBraces}`, Value: `${before}${variableWithBraces}${after}` };
+  });
+  autoCompleteSetter(suggestions);
+};
 
 
 function getCaretPosition(textarea: HTMLTextAreaElement) {
-    if (textarea.parentNode == null) {return [0, 0]}
-    const { selectionStart } = textarea;
-    const hiddenDiv = document.createElement('div')
-    const style = getComputedStyle(textarea);
+  if (textarea.parentNode == null) {return [0, 0]}
+  const { selectionStart } = textarea;
+  const hiddenDiv = document.createElement('div')
+  const style = getComputedStyle(textarea);
 
-    Array.from(style).forEach((propertyName) => {
-        const value = style.getPropertyValue(propertyName);
-        hiddenDiv.style.setProperty(propertyName, value);
-    })
+  Array.from(style).forEach((propertyName) => {
+    const value = style.getPropertyValue(propertyName);
+    hiddenDiv.style.setProperty(propertyName, value);
+  })
 
-    // Set text content up to caret
-    const beforeCaret = textarea.value.substring(0, selectionStart);
-    const toLastOpenBracket = "\n" + beforeCaret.split('{').slice(0, -1).join('{');
-    const afterCaret = textarea.value.substring(selectionStart) ?? '.';
+  // Set text content up to caret
+  const beforeCaret = textarea.value.substring(0, selectionStart);
+  const toLastOpenBracket = "\n" + beforeCaret.split('{').slice(0, -1).join('{');
+  const afterCaret = textarea.value.substring(selectionStart) ?? '.';
 
-    hiddenDiv.textContent = toLastOpenBracket;
+  hiddenDiv.textContent = toLastOpenBracket;
 
-    // Create a span to mark caret position
-    const span = document.createElement('span');
-    span.textContent = afterCaret[0];
-    hiddenDiv.appendChild(span);
+  // Create a span to mark caret position
+  const span = document.createElement('span');
+  span.textContent = afterCaret[0];
+  hiddenDiv.appendChild(span);
 
-    textarea.parentNode.appendChild(hiddenDiv);
+  textarea.parentNode.appendChild(hiddenDiv);
 
-    // Get caret's vertical position relative to textarea
-    const caretX = span.offsetLeft - textarea.scrollLeft;
-    const caretY = span.offsetTop - textarea.scrollTop;
+  // Get caret's vertical position relative to textarea
+  const caretX = span.offsetLeft - textarea.scrollLeft;
+  const caretY = span.offsetTop - textarea.scrollTop;
 
-    textarea.parentNode.removeChild(hiddenDiv);
-    return [caretX, caretY];
+  textarea.parentNode.removeChild(hiddenDiv);
+  return [caretX, caretY];
 }
     
