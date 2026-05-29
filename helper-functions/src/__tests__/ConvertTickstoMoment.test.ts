84b7d9d2708a1c5ded755e9d938cc4fcedf39a39
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2026-05-29 10:13:29 -0400

    Fixed Unit Tests
    Co-authored-by: Preston Crawford <78245034+prestoncraw@users.noreply.github.com>
    Signed-off-by: Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>

--- helper-functions/src/__tests__/ConvertTickstoMoment.test.ts
+++ helper-functions/src/__tests__/ConvertTickstoMoment.test.ts
@@ -2,12 +2,12 @@ import { ConvertTicksToMoment } from '../index';
 
 test('638712864000000000', () => {
     const d = 638712864000000000;
-    expect(ConvertTicksToMoment(d)).toMatch(1735689600000);
+    expect(ConvertTicksToMoment(d)).toBe(1735689600000);
 });
 test('0 Ticks', () => {
-    expect(ConvertTicksToMoment(0)).toMatch(-62167219200000);
+    expect(ConvertTicksToMoment(0)).toBe(-62167219200000);
 });
 test('Unix Timestamp in Ticks', () => {
     const d = 621355968000000000;
-    expect(ConvertTicksToMoment(d)).toMatch(0);
+    expect(ConvertTicksToMoment(d)).toBe(0);
 });
