242bcaa68c4df15f12672d1802e55abff03142fe
prestoncraw <prestoncrawford530@gmail.com>
2026-06-26 12:49:40 -0400

    Add HeartBeatCheck component

--- common-pages/src/index.tsx
+++ common-pages/src/index.tsx
@@ -42,6 +42,7 @@ import ReadOnlyGenericSlice from './Gemstone/GenericSlices/ReadOnlyGenericSlice'
 import ReadWriteGenericSlice from './Gemstone/GenericSlices/ReadWriteGenericSlice';
 import { useInitializeWithFetch } from './Gemstone/GenericSlices/useInitializeData';
 import { ReadOnlyControllerFunctions, ReadWriteControllerFunctions } from './Gemstone/ControllerFunctions';
+import HeartBeatCheck from './HeartBeatCheck';
 import useSearchData from './Gemstone/Hooks/useSearchData';
 
 const Pipelines = {
@@ -69,6 +70,7 @@ export {
   IStartDuration,
   IEndDuration,
   Gemstone,
+  HeartBeatCheck,
 
   //.NET Gemstone related exports
   ReadOnlyGenericSlice as ReadOnlyGenericSlice_Gemstone,
